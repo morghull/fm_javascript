14f5a63de47c149b64e12e48efb2a68fbd9ae284
morghull <030242@iskra.local>
2021-09-27 11:00:04 +0300

    working with incremets and decrements

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let someVariable = "someValue";
+let iter = 5;
 
-let messUser = 'Маша говорит: \'Привет\'   \u2721';
+iter++;
 
-console.log(messUser);
+console.log(iter);
+
+let a = 1;
+let b = 2;
+
+let c = ++a + ++a + ++b;
+
+console.log("c=", c);
+console.log("a=", a);
+console.log("b=", b);
