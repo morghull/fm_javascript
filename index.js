c813078c93e26bc34a7707d942b83feac548ae8b
morghull <030242@iskra.local>
2021-09-28 11:08:09 +0300

    add function with bad code

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-alert("I'm teacher");
-let userInput = prompt('Enter number: ');
-
-let isNotNumber =
-  userInput === '' ||
-  userInput === null ||
-  isNaN(Number(userInput));
-
-if (isNotNumber) {
-  console.log('error');
-} else {
-  console.log('well done');
+// oldschool
+myFirstFunction();
+function myFirstFunction() {
+  console.log('text in function 1');
 }
+myFirstFunction();
 
-// console.log(userInput);
-// console.log(isNaN(Number(userInput)));
-// console.log(typeof Number(userInput));
+//function expression
+//mySecondFunction(); returns error
+const mySecondFunction = function () {
+  console.log('text in function 2');
+};
+mySecondFunction();
+
+console.log(mySecondFunction);
+
+const getSumTwoNums = function () {
+  let userInput1 = prompt('enter first number');
+  let userInput1 = prompt('enter second number');
+
+  let checkEmpty =
+    userInput1 === '' ||
+    userInput1 == null ||
+    userInput2 === '' ||
+    userInput2 == null;
+  let sum = Number(userInput) + Number(userInput2);
+
+  if (checkEmpty || isNaN(sum)) {
+    console.log('error');
+  } else {
+    console.log(userInput1, '+', userInput2, '=', sum);
+  }
+};
+
+getSumTwoNums();
