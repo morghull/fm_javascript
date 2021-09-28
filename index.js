7cb6d9413473c237fef4e11037d9b8b2933a07ad
morghull <030242@iskra.local>
2021-09-28 11:39:16 +0300

    add function with js docs

--- index.js
+++ index.js
@@ -14,22 +14,48 @@ mySecondFunction();
 
 console.log(mySecondFunction);
 
-const getSumTwoNums = function () {
-  let userInput1 = prompt('enter first number');
-  let userInput1 = prompt('enter second number');
+//   let checkEmpty =
+//     userInput1 === '' ||
+//     userInput1 == null ||
+//     userInput2 === '' ||
+//     userInput2 == null;
 
-  let checkEmpty =
-    userInput1 === '' ||
-    userInput1 == null ||
-    userInput2 === '' ||
-    userInput2 == null;
-  let sum = Number(userInput) + Number(userInput2);
-
-  if (checkEmpty || isNaN(sum)) {
-    console.log('error');
-  } else {
-    console.log(userInput1, '+', userInput2, '=', sum);
-  }
+/**
+ *
+ * @param {string} value
+ * @returns {boolean}
+ */
+const isNotNumber = function (value) {
+  return (
+    value === '' || value === null || isNaN(Number(value))
+  );
 };
 
-getSumTwoNums();
+/**
+ *
+ * @param {any} num1
+ * @param {any} num2
+ * @returns {boolean | number}
+ */
+const getSumTwoNums = function (num1, num2) {
+  if (isNotNumber(num1) || isNotNumber(num2)) {
+    return false;
+  }
+  return Number(num1) + Number(num2);
+};
+
+let userInput1 = prompt('enter first number');
+let userInput1 = prompt('enter second number');
+
+let result = getSumTwoNums(userInput1, userInput1);
+if (result === false) {
+  console.log('error');
+} else {
+  console.log(result);
+}
+
+// if (checkEmpty || isNaN(sum)) {
+//   console.log('error');
+// } else {
+//   console.log(userInput1, '+', userInput2, '=', sum);
+// }
