c95fdfe7f29cadfa426ad71a86671d2cfeeffc06
morghull <030242@iskra.local>
2021-09-28 09:19:11 +0300

    comparison

--- index.js
+++ index.js
@@ -1,47 +1,13 @@
-let number = 12;
-
-//number = number + 10;
-
-number += 10; //22
-
-number %= 5; //2
-
-number **= 3; //8
-
-number /= number; //1
-
-number *= 4; //4
-
-let nameUser = "Elon";
-nameUser += +" Musk";
-console.log(nameUser);
-//ElonNaN
-
-let firstVariable = 45;
-let secondVariable = 12;
-let result;
-
-result = firstNumber + secondNumber;
-console.log(result);
-result = firstNumber - secondNumber;
-console.log(result);
-result = firstNumber * secondNumber;
-console.log(result);
-result = firstNumber % secondNumber;
-console.log(result);
-
-{
-  let someRandomNumber = 101;
-  let logicalValue = true;
-  let nameOfProgramLanguage = "Java";
-  let millionaireName = "Elon Musk";
-  let dataTable = null;
-
-  let number = 23;
-  number += 11;
-  number /= 2;
-  --number;
-  number *= 11;
-  ++number;
-  number %= 5;
-}
+8 == 8 //true
+8 != 8 //false
+8 === 8 //true
+8 !== 8 //false
+8 == '8' //true
+8 === '8' //false
+8 != '8' //false
+8 !== '8' //true
+8 === +'8' //true
+8 === Number('8') //true
+NaN === NaN //false
+null === null //true
+undefined === undefined //true
