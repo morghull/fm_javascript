0ea5c1a88e85371c8da9e91b1d735f8cbf6291d1
morghull <030242@iskra.local>
2021-09-29 11:04:18 +0300

    example of default values for parameters

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const convertToNumber = function (value) {
-  if (
-    value === '' ||
-    value === null ||
-    isNaN(Number(value))
-  ) {
-    return NaN;
-  }
-  return Number(value);
+const logWord = function (word = 'sample',sign='!') {
+  console.log(word,sign);
 };
-
-const isAdult = function (age, AGE_ADULT = 18) {
-  if (isNaN(age)) return null;
-  return age >= AGE_ADULT;
-};
-
-let age = '17';
-let changedAge = convertToNumber(age);
-console.log(isAdult(changedAge));
-
-console.log(isAdult(convertToNumber(20)));
-console.log(isAdult(convertToNumber(2)));
-console.log(isAdult(convertToNumber(18)));
-console.log(isAdult(convertToNumber(17)));
+logWord();
+debugger;
+logWord('Qwerty');
+logWord(undefined,'?');
