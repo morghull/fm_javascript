9a26ca77222ef3f36ff09e8405ac9cdbdd484e37
morghull <030242@iskra.local>
2021-09-29 10:56:01 +0300

    practice with functions

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-//let sideSquare = prompt('Enter side of square', '10');
-
-const getPerimeterSquare = function (side) {
-  if (isNaN(a)) {
-    return null;
-  }
-  return 4 * side;
-};
-
-const isNumber = function (value) {
+const convertToNumber = function (value) {
   if (
     value === '' ||
     value === null ||
@@ -18,7 +9,16 @@ const isNumber = function (value) {
   return Number(value);
 };
 
-let sideSquare = 'qwe';
-console.log(getPerimeterSquare(3));
-let changeSidSquare = isNumber(sideSquare);
-console.log(getPerimeterSquare(changeSidSquare));
+const isAdult = function (age, AGE_ADULT = 18) {
+  if (isNaN(age)) return null;
+  return age >= AGE_ADULT;
+};
+
+let age = '17';
+let changedAge = convertToNumber(age);
+console.log(isAdult(changedAge));
+
+console.log(isAdult(convertToNumber(20)));
+console.log(isAdult(convertToNumber(2)));
+console.log(isAdult(convertToNumber(18)));
+console.log(isAdult(convertToNumber(17)));
