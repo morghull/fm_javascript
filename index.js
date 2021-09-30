4c521a8c0751c38050e31e799c345299ed0728ab
morghull <030242@iskra.local>
2021-09-30 11:31:52 +0300

    add practice with switch and else ifs

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-let languageOption = '4';
+let monthInput = '12';
+let expectedMonthNumber = monthInput;
 
-switch (languageOption) {
+switch (expectedMonthNumber) {
   case '1':
-    console.log('ru-default');
-    break;
   case '2':
-    console.log('ru');
+  case '12':
+    console.log('winter');
     break;
   case '3':
-    console.log('fr');
-    break;
   case '4':
-    console.log('en');
+  case '5':
+    console.log('spring');
+    break;
+  case '6':
+  case '7':
+  case '8':
+    console.log('summer');
+    break;
+  case '9':
+  case '10':
+  case '11':
+    console.log('fall');
     break;
   default:
-    console.log('indalid input');
+    console.log('invalid data');
     break;
 }
-console.log('thanks');
+
+if (isNaN(expectedMonthNumber)) {
+  console.log('invalid input');
+} else if (['1', '2', '12'].includes(expectedMonthNumber)) {
+  console.log('winter');
+} else if (['3', '4', '5'].includes(expectedMonthNumber)) {
+  console.log('spring');
+} else if (['6', '7', '8'].includes(expectedMonthNumber)) {
+  console.log('summer');
+} else if (
+  ['9', '10', '11'].includes(expectedMonthNumber)
+) {
+  console.log('fall');
+} else {
+  console.log('invalid data');
+}
