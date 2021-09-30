fbbed246b518c44dbf3180e4df0b2675d527f406
morghull <030242@iskra.local>
2021-09-30 12:02:08 +0300

    add calculate function

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-let monthInput = '12';
-let expectedMonthNumber = monthInput;
-
-switch (expectedMonthNumber) {
-  case '1':
-  case '2':
-  case '12':
-    console.log('winter');
-    break;
-  case '3':
-  case '4':
-  case '5':
-    console.log('spring');
-    break;
-  case '6':
-  case '7':
-  case '8':
-    console.log('summer');
-    break;
-  case '9':
-  case '10':
-  case '11':
-    console.log('fall');
-    break;
-  default:
-    console.log('invalid data');
-    break;
-}
-
-if (isNaN(expectedMonthNumber)) {
-  console.log('invalid input');
-} else if (['1', '2', '12'].includes(expectedMonthNumber)) {
-  console.log('winter');
-} else if (['3', '4', '5'].includes(expectedMonthNumber)) {
-  console.log('spring');
-} else if (['6', '7', '8'].includes(expectedMonthNumber)) {
-  console.log('summer');
-} else if (
-  ['9', '10', '11'].includes(expectedMonthNumber)
+const sum = function (a, b) {
+  return a + b;
+};
+const sub = function (a, b) {
+  return a - b;
+};
+const mult = function (a, b) {
+  return a * b;
+};
+const div = function (a, b) {
+  return a / b;
+};
+const rem = function (a, b) {
+  return a % b;
+};
+const calculate = function (
+  num1 = 1,
+  num2 = 2,
+  operator = '+'
 ) {
-  console.log('fall');
-} else {
-  console.log('invalid data');
-}
+  if (isNaN(num1 * num2)) {
+    return false;
+  }
+  let f = null;
+  switch (operator) {
+    case '+':
+      f = sum;
+      break;
+    case '-':
+      f = sub;
+      break;
+    case '*':
+      f = mult;
+      break;
+    case '/':
+      f = div;
+      break;
+    case '/':
+      f = rem;
+      break;
+    // default:
+    //   return null;
+  }
+  return typeof f === 'function' ? f(num1, num2) : f;
+};
+
+console.log(calculate());
+console.log(calculate(2, 8, '*'));
