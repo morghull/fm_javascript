c1d4c3c54a116267100c8f456465171d56231613
morghull <030242@iskra.local>
2021-09-30 10:31:35 +0300

    working with else if construction

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const isAdult = function (age) {
-  return (age >= 18 ? '' : 'not ') + 'adult';
-};
+// const isAdult = function (age) {
+//   return (age >= 18 ? '' : 'not ') + 'adult';
+// };
 
-console.log(isAdult(18));
-console.log(isAdult(12));
+// console.log(isAdult(18));
+// console.log(isAdult(12));
 
-//function with conditional operator
-const messageCheck = function (message = '') {
-  let isValid = typeof message === typeof '' && message;
-  return isValid ? 'thank you' : 'invalid message';
-};
-let userMessage = prompt('Type something, please: ');
-console.log(messageCheck(userMessage));
-console.log(messageCheck(8));
+// //function with conditional operator
+// const messageCheck = function (message = '') {
+//   let isValid = typeof message === typeof '' && message;
+//   return isValid ? 'thank you' : 'invalid message';
+// };
+// let userMessage = prompt('Type something, please: ');
+// console.log(messageCheck(userMessage));
+// console.log(messageCheck(8));
+
+//
+let userInput = 9;
+let expectedNumber = +userInput;
+
+if (isNaN(expectedNumber)) {
+  console.log('invalid input');
+} else if (expectedNumber > 0) {
+  if (expectedNumber % 5 === 0) {
+    console.log('can be devided by 5');
+  } else if (expectedNumber % 3 === 0) {
+    console.log('can be devided by 3');
+  } else if (expectedNumber % 2 === 0) {
+    console.log('can be devided by 2');
+  }
+} else {
+  console.log('negative number, must be positive');
+}
