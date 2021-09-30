2001890e8211b1f3f3cb5b3829a119e51c8e1997
morghull <030242@iskra.local>
2021-09-30 11:03:48 +0300

    working with switch

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
-// const isAdult = function (age) {
-//   return (age >= 18 ? '' : 'not ') + 'adult';
-// };
+let languageOption = '4';
 
-// console.log(isAdult(18));
-// console.log(isAdult(12));
-
-// //function with conditional operator
-// const messageCheck = function (message = '') {
-//   let isValid = typeof message === typeof '' && message;
-//   return isValid ? 'thank you' : 'invalid message';
-// };
-// let userMessage = prompt('Type something, please: ');
-// console.log(messageCheck(userMessage));
-// console.log(messageCheck(8));
-
-//
-let userInput = 9;
-let expectedNumber = +userInput;
-
-if (isNaN(expectedNumber)) {
-  console.log('invalid input');
-} else if (expectedNumber > 0) {
-  if (expectedNumber % 5 === 0) {
-    console.log('can be devided by 5');
-  } else if (expectedNumber % 3 === 0) {
-    console.log('can be devided by 3');
-  } else if (expectedNumber % 2 === 0) {
-    console.log('can be devided by 2');
-  }
-} else {
-  console.log('negative number, must be positive');
+switch (languageOption) {
+  case '1':
+    console.log('ru-default');
+    break;
+  case '2':
+    console.log('ru');
+    break;
+  case '3':
+    console.log('fr');
+    break;
+  case '4':
+    console.log('en');
+    break;
+  default:
+    console.log('indalid input');
+    break;
 }
+console.log('thanks');
