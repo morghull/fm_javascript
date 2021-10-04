c50c8d595b03ad9d91d37aefd20b7534b7774898
morghull <030242@iskra.local>
2021-10-04 09:22:21 +0300

    add basic object

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-const toNumber = function (number) {
-  if (isNaN(number) || number === null) return null;
-  return Number(number);
-};
-const factorialCalc = function (number = 3) {
-  if (number > MAX_FACTORIAL_BASIS) return false;
-  if (number < 0) return null;
-  let result = 1n;
-  for (let i = 1; i <= number; i++) {
-    result *= BigInt(i);
-  }
-  return result;
+const object = {
+  display: 'flex',
+  margin: 15,
+  justifyContent: 'center',
+  visibility: true,
+  resize: function () {
+    return 'resize';
+  },
 };
 
-console.log(factorialCalc(toNumber()));
-console.log(factorialCalc(toNumber(3)));
-console.log(factorialCalc(toNumber(4)));
-console.log(factorialCalc(toNumber('4')));
-console.log(factorialCalc(toNumber(null)));
-console.log(factorialCalc(toNumber('0')));
-console.log(factorialCalc(toNumber(1)));
-console.log(factorialCalc(toNumber(-9)));
-console.log(factorialCalc(toNumber('qweasd')));
-console.log(factorialCalc(toNumber(555)));
