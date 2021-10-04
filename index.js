adb428837b0defc6ee36b07c829163c158b103c3
morghull <030242@iskra.local>
2021-10-04 09:42:49 +0300

    add cats

--- index.js
+++ index.js
@@ -8,3 +8,47 @@ const object = {
   },
 };
 
+const cat = {
+  name: 'Muha',
+  isMale: false,
+  color: 'gray',
+  breed: 'yard',
+  age: 2,
+  weight: 3,
+  isSleeping: true,
+  say: function () {
+    return 'meow';
+  },
+  run: function () {
+    return 'running';
+  },
+};
+
+cat.legsCount = 4;
+cat.newProp = function () {};
+
+console.log(cat);
+
+cat.age++;
+delete cat.isSleeping;
+console.log(cat.say());
+
+console.log(cat);
+console.log(cat.name);
+
+const cat = {
+  name: 'Jirobasik',
+  isMale: true,
+  color: 'bandicoon',
+  age: 3,
+  weight: 6,
+  isSleeping: false,
+  say: function () {
+    return 'meow meow';
+  },
+  run: function () {
+    return 'walking';
+  },
+};
+
+
