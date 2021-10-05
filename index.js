b6bf694540f9b3eed4eff2c77cc4f5ce56a17e74
morghull <030242@iskra.local>
2021-10-05 14:35:19 +0300

    add practice with objects and functions

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const obj = {
-  name: 'pen',
-  'is written': true,
-  11: 78,
+const createMultiplicationTable = function (
+  min = 33,
+  max = 35
+) {
+  const table = {};
+  for (let i = min; i <= max; i++) {
+    for (let j = min; j <= max; j++) {
+      table[`${i} * ${j} = `] = i * j;
+    }
+  }
+  return table;
 };
 
-const propertyName = 'is written';
+console.log(createMultiplicationTable());
 
-console.log(obj.name);
-console.log(obj[propertyName]);
+const getResultOfMultiplication = function (
+  expression,
+  table
+) {
+  return table[expression];
+};
+const table = createMultiplicationTable(1, 10);
+console.log(getResultOfMultiplication('2 * 2 = ', table));
+console.log(getResultOfMultiplication('3 * 8 = ', table));
+console.log(getResultOfMultiplication('4 * 5 = ', table));
+console.log(getResultOfMultiplication('2 * 9 = ', table));
