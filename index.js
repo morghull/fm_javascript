2d7e47de3266bfea873cc449599b505c49ec301e
morghull <030242@iskra.local>
2021-10-06 11:27:45 +0300

    add custom prototype

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-// function t(){};
-
-// console.dir(t);
-
-// console.dir(function(){});
-
-function MyArray() {
-  this.length = 0;
-  
+/*logic*/ 
+function MyArrayProto() {
   this.push = function (item) {
     this[this.length] = item;
-    this.length++;
-    return this.length;
+    return ++this.length;
   };
 }
+/*data*/
+function MyArray() {
+  this.length = 0;
+}
+/*prototype*/
+MyArray.prototype = new myArrayProto();
 
 const myArray = new MyArray();
 const myArray2 = new MyArray();
-console.log(myArray.push === myArray2.push);//false
+console.log(myArray.push === myArray2.push); //false
 
 const arr = [];
 arr.push(1);
 arr.push(12);
 
 const arr2 = [];
-console.log(arr.push === arr2.push);//true
+console.log(arr.push === arr2.push); //true
