39747a17d620ee0448e5917f140f3bc00125c140
morghull <030242@iskra.local>
2021-10-08 09:25:38 +0300

    add HOF function

--- js/index.js
+++ js/index.js
@@ -1,24 +1,10 @@
-function LadderPrototype() {
-  this.up = function () {
-    this.value ++;
-    return this;
-  };
-  this.down = function () {
-    this.value --;
-    if (this.value <= 0) this.value = 0;
-    return this;
-  };
-  this.showLevel = function () {
-    return this.value;
-  };
+/**
+ * 
+ * @param {function} howSay 
+ * @param {string} whatSay 
+ */
+function saySomething(howSay, whatSay) {
+  howSay(whatSay);
 }
-function Ladder() {
-  this.value = 0;
-  if (!new.target) return new Ladder();
-}
-Ladder.prototype = new LadderPrototype();
 
-const ld = new Ladder();
-console.log(ld.showLevel());
-ld.up().up().up().up().up().down();
-console.log(ld.showLevel());
+saySomething(console.log,'some');
