dccea9f19df063fcb46075207e95db272e048efc
morghull <030242@iskra.local>
2021-10-08 11:04:45 +0300

    MyArray: add reverse, unshift,slice,includes meths

--- js/MyArray.js
+++ js/MyArray.js
@@ -13,11 +13,38 @@ function MyArrayProto() {
     }
     return this.length;
   };
+  this.unshift = function () {
+    for (let i = arguments.length - 1; i >= 0; i--) {
+      if (
+        arguments[i] instanceof MyArray ||
+        arguments[i].constructor === Array
+      ) {
+        for (let j = arguments[i].length - 1; j >= 0; j--) {
+          let value = arguments[i][j];
+          for (let k = 0; k <= this.length; k++) {
+            const buffer = this[k];
+            this[k] = value;
+            value = buffer;
+          }
+          this.length++;
+        }
+      } else {
+        let value = arguments[i];
+        for (let j = 0; j <= this.length; j++) {
+          const buffer = this[j];
+          this[j] = value;
+          value = buffer;
+        }
+        this.length++;
+      }
+    }
+    return this.length;
+  };
   this.reverse = function () {
     let i = 0;
     let j = this.length - 1;
     while (i < j) {
-      let buffer = this[i];
+      const buffer = this[i];
       this[i] = this[j];
       this[j] = buffer;
       i++;
@@ -52,6 +79,30 @@ function MyArrayProto() {
     }
     return false;
   };
+  this.forEach = function (func) {
+    for (let i = 0; i < this.length; i++) {
+      func(this[i]);
+    }
+  };
+  this.some = function (func) {
+    for (let i = 0; i < this.length; i++) {
+      if (func(this[i])) return true;
+      return false;
+    }
+  };
+  this.every = function (func) {
+    for (let i = 0; i < this.length; i++) {
+      if (!func(this[i])) return false;
+    }
+    return true;
+  };
+  this.filter = function (func) {
+    const result = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      if (func(this[i])) result.push(this[i]);
+    }
+    return result;
+  };
 }
 /*data*/
 function MyArray() {
@@ -70,11 +121,21 @@ Array.prototype.newMethod = function () {
   return 'new method';
 };
 
-const task1Array1 = new MyArray([1, 2, 3]);
-const task1Array2 = new MyArray(4, 5, 6);
-const task1ConcatArray = new MyArray().concat(
-  task1Array1,
-  task1Array2
+const concatArray1 = new MyArray([1, 2, 3]);
+const concatArray2 = new MyArray(4, 5, 6);
+const concatedArray = new MyArray().concat(
+  concatArray1,
+  concatArray2
 );
-console.log('concat:', task1ConcatArray);
-console.log('reverse:', task1Array1.reverse());
+console.log('concat:', concatedArray);
+
+const reverseArray = new MyArray(1, 2, 3);
+console.log('reverse:', reverseArray.reverse());
+
+const pushArray = new MyArray(1, 2, 3);
+pushArray.push(4,[5],new MyArray(6));
+console.log('push:', pushArray);
+
+const unshiftArray = new MyArray(1, 2, 3);
+unshiftArray.unshift(4,[5],new MyArray(6));
+console.log('unshift:', unshiftArray);
