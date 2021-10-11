9f8914bf9ab639bdc8cc9508fd633fbcce19f2b7
morghull <morghull@gmail.com>
2021-10-11 10:52:41 +0300

    add arrow function

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
 'use strict';
 
-function logItem(header, index) {
-    console.log(this.title);
-    console.log(that.title);
-    console.log(`header ${index}: ${header}`);
-    const test = () => {
-      console.log(this);
-    };
-    test();
-  }
-
 const site = {
   title: 'Green site',
   headers: ['Header1', 'Test', 'Header3', 'New article'],
   showHeaders() {
     console.log(this);
-    this.headers.forEach(logItem.bind(this));
+    this.headers.forEach((header, index) =>{
+        console.log(this.title);
+        console.log(that.title);
+        console.log(`header ${index}: ${header}`);
+        const test = () => {
+          console.log(this);
+        };
+        test();
+      });
   },
 };
 
