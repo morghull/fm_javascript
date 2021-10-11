28bffa30ba3f6ed88084b5c5677fc8ba64249f46
morghull <morghull@gmail.com>
2021-10-11 11:33:51 +0300

    add recursion

--- js/index.js
+++ js/index.js
@@ -1,40 +1,11 @@
 'use strict';
 
-const t1 = function (...qq) {
-  console.log(arguments);
-  console.log(qq);
-};
+function recursion(num) {
+  if (num === 0) {
+    return;
+  }
+  console.log(num);
+  recursion(num - 1);
+}
 
-const t2 = (num, ...rest) => {
-  console.log('num=', num);
-  console.log('rest=', rest);
-};
-
-console.dir(t1);
-console.dir(t2);
-t1(1, 2, 3, 4);
-t2(1, 2, 3, 4);
-
-const arrFunc1 = (n) => {
-  return n * n;
-};
-const arrFunc2 = (n) => n * n;
-const arrFunc3 = (n) => n * n;
-
-const infinitySum = (...args) => {
-  let summ = 0;
-  args.forEach((item) => (summ += item));
-  return summ;
-};
-console.log(infinitySum(1, 23, 4));
-
-const infinitySum2 = (...args) =>
-  args.reduce((result, n) => result + n);
-
-const arrNums1 = [4, 1, 3, 6];
-const arrNums2 = [2, 5, 7, 9];
-console.log(infinitySum(...arrNums1));
-console.log(infinitySum2(...arrNums1));
-
-const arr3 = [...arrNums1, 101, 256, ...arrNums2];
-console.log(arr3);
+recursion(4);
