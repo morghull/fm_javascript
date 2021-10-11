4baf04b9f926b8f261e4c93a67bd5f4a2a5f4d31
morghull <morghull@gmail.com>
2021-10-11 12:02:57 +0300

    add working with recursion

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
 'use strict';
 
-function recursion(num) {
-  if (num === 0) {
-    return;
+const power = (num, pow) => {
+  if (pow === 1) {
+    return num;
   }
-  console.log(num);
-  recursion(num - 1);
-}
+  return num * power(num, pow - 1);
+};
 
-recursion(4);
+console.log(power(2, 2));
+console.log(power(2, 3));
+console.log(power(2, 4));
+
+const factorial = (num) => {
+  if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+};
+
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
