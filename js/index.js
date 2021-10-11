d1f1b59133c3660baf764899369b27cd1ec81ade
morghull <morghull@gmail.com>
2021-10-11 11:15:49 +0300

    add sum with rest arguments

--- js/index.js
+++ js/index.js
@@ -1,7 +1,32 @@
 'use strict';
 
-const sum = (a, b) => a + b;
-console.log(sum(1, 2));
+const t1 = function (...qq) {
+  console.log(arguments);
+  console.log(qq);
+};
 
-const square = (n) => n * n;
-console.log(square(sum(1, 2)));
+const t2 = (num, ...rest) => {
+  console.log('num=', num);
+  console.log('rest=', rest);
+};
+
+console.dir(t1);
+console.dir(t2);
+t1(1, 2, 3, 4);
+t2(1, 2, 3, 4);
+
+const arrFunc1 = (n) => {
+  return n * n;
+};
+const arrFunc2 = (n) => n * n;
+const arrFunc3 = (n) => n * n;
+
+const infinitySum = (...args) => {
+  let summ = 0;
+  args.forEach((item) => (summ += item));
+  return summ;
+};
+console.log(infinitySum(1, 23, 4));
+
+const infinitySum2 = (...args) =>
+  args.reduce((result, n) => result + n);
