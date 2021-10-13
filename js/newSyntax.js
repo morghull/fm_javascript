cf10edce016c961c29edf7d8638558095aafa29d
morghull <morghull@gmail.com>
2021-10-13 13:36:20 +0300

    add getter and setters

--- js/newSyntax.js
+++ js/newSyntax.js
@@ -8,15 +8,21 @@ class UserClass {
    * @param {number} age
    */
   constructor(name, sname, age) {
+    this.name = name;
+    this.sname = sname;
+    this.age = age;
+  }
+  get age() {
+    return this._age;
+  }
+  set age(age) {
     if (typeof age !== 'number')
       throw new TypeError('Age mus be a number');
     if (age < 0 || age > MAX_AGE)
       throw new RangeError(
         `Age must be >0 and <${MAX_AGE}`
       );
-    this.name = name;
-    this.sname = sname;
-    this.age = age;
+    this._age = age;
   }
   getFullName() {
     return `${this.name} ${this.sname}`;
@@ -28,6 +34,8 @@ class UserClass {
 
 const u2 = new UserClass('Tim', 'Lee', 50);
 const u3 = new UserClass('Bruse', 'Lee', 75);
+u2.age = 49; //setter
+console.log(u2.age); //getter
 
 class Employee {
   constructor(fname, lname, rate) {
