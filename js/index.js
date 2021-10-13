98d75f6aabf978de6185f097f5bff56c76be3d0b
morghull <morghull@gmail.com>
2021-10-13 10:08:38 +0300

    add new Error

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
-const str = 'to    be     or        not to be';
-
-const capitalize = (input) =>
-  input
-    .replace('  ', ' ')
-    .split(' ')
-    .map(
-      (word) =>
-        word[0].toUpperCase() + word.slice(1).toLowerCase()
-    )
-    .join(' ');
-
-console.log(capitalize('asd asd as dasd asd s'));
+const pow = (num, pow) => {
+  if (typeof num !== 'number' || typeof pow !== 'number') {
+    return new TypeError('Value must be number');
+  }
+  if (pow < 0) {
+    return new RangeError(
+      'Invalid value pow. Pow must be > 0'
+    );
+  }
+  if (pow === 0) return 1;
+  return num * pow(num, pow);
+};
+console.log(pow('ten', 2));
+console.log(pow(2, -2));
