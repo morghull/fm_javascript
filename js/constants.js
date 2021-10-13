c042a1ee8f726adf411d9335a205920d8f3af9ed
morghull <morghull@gmail.com>
2021-10-13 13:27:22 +0300

    add validation to UserClass

--- js/constants.js
+++ js/constants.js
@@ -3,3 +3,4 @@ const MAX_COUNT_TRY = 3;
 const MAX_FACTORIAL_BASIS = 100;
 const RETIRE_AGE = 65;
 const ADULT_AGE = 18;
+const MAX_AGE = 150;
--- js/newSyntax.js
+++ js/newSyntax.js
@@ -1,7 +1,19 @@
 'use strict';
 
 class UserClass {
+  /**
+   *
+   * @param {string} name
+   * @param {string} sname
+   * @param {number} age
+   */
   constructor(name, sname, age) {
+    if (typeof age !== 'number')
+      throw new TypeError('Age mus be a number');
+    if (age < 0 || age > MAX_AGE)
+      throw new RangeError(
+        `Age must be >0 and <${MAX_AGE}`
+      );
     this.name = name;
     this.sname = sname;
     this.age = age;
@@ -14,4 +26,19 @@ class UserClass {
   }
 }
 
-const u2 = new UserClass('Tim', 'Lee', 65);
+const u2 = new UserClass('Tim', 'Lee', 50);
+const u3 = new UserClass('Bruse', 'Lee', 75);
+
+class Employee {
+  constructor(fname, lname, rate) {
+    this.fname = fname;
+    this.lname = lname;
+    this.rate = rate;
+  }
+  getSalary(dayCount) {
+    return dayCount * this.rate;
+  }
+}
+
+const emp1 = new Employee('Elon', 'Musk', 50);
+console.log(emp1.getSalary(30));
