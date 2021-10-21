ba6adedabdd02fda761cdde32846af6573a8dec8
morghull <morghull@gmail.com>
2021-10-21 11:47:19 +0300

    refactor Stack

--- js/Stack.js
+++ js/Stack.js
@@ -33,15 +33,23 @@ class Stack {
 
 const stack1 = new Stack(3, 77, 78, 79);
 
-const checkSequence = (str) => {
+const checkSequence = (str, options) => {
+  const brackets = options.brackets;
+  const closeBrackets = Object.values(brackets);
   const stack = new Stack(str.length);
-  const openQuotes = ['(', '{', '['];
-  const quotesPair = { '(': ')', '{': '}', '[': ']' };
   for (const char of str) {
-    if (openQuotes.includes(char)) stack.push(char);
-    else if (quotesPair[`${stack.pick()}`] === char) stack.pop();
-    else return false;
+    if (brackets[char]) stack.push(char);
+    else if (brackets[stack.pick()] === char) stack.pop();
+    else if (closeBrackets.includes(char)) return false;
   }
   return stack.isEmpty;
 };
-console.log(checkSequence('{((()))}'));
+
+const options = {
+  brackets: {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  },
+};
+console.log(checkSequence('(5+2)*(7-5)-2.0/{44}', options));
